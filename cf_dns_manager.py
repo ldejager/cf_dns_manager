fe00d91fb78d4bb1bc0e82a1813c0df92e06b3b4
Leon de Jager <leondejager@gmail.com>
2014-07-20 08:28:14 +0100

    Defining class and init

--- cf_dns_manager.py
+++ cf_dns_manager.py
@@ -2,4 +2,28 @@
 #
 # Similar to cf_update_dns but in python
 
+import json
+import requests
+import sys
+from datetime import datetime
+
+CF_TOKEN = ''
+CF_EMAIL = ''
+CF_DZONE = ''
+
+
+class CloudFlare(object):
+
+    API_URL = 'https://www.cloudflare.com/api_json.html'
+
+    def __init__(self, api_key, cf_email):
+        self.tkn = api_key
+        self.email = cf_email
+
+
+
+
+
+
+
 
